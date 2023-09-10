654f3ed7076103962672ff9035f3aa44d240d48e
EzekielEnnis <143122905+EzekielEnnis@users.noreply.github.com>
2023-09-10 19:39:54 +0100

    Add basic logic for changing grid
    Add functions gridAppend and gridRemove to respectivly
    add or remove boxes from the grid and basic logic within
    them to determine the amount. As well as logic for the
    slider, detecting wether the grid needs to be added to
    or removed from.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+
+const container = document.querySelector(".sketch-container");
+
 function createGrid(size){
     let gridSize = size*size;
 
 // find percentage of space the box should take up (basis)
     let basis = (size/gridSize)*100; 
-    const container = document.querySelector(".sketch-container");
     for (let i = 0; i < gridSize; i++) {
         const box = document.createElement("div");
         box.classList.add("sketch-box");
@@ -12,6 +14,26 @@ function createGrid(size){
         container.appendChild(box);
     };
 };
+
+// Append box to grid
+function girdAppend(currentSize, newSize) {
+    // How many boxes need to be added
+    let dif = (newSize*newSize)-(currentSize*currentSize)
+    for (let i = 0; i < dif; i++) {
+        const element = array[i];
+        // Append box
+    }
+}
+
+// Remove box from grid
+function girdAppend(currentSize, newSize) {
+    // How many boxes need to be added
+    let dif = (newSize*newSize)-(currentSize*currentSize)
+    for (let i = 0; i < dif; i++) {
+        const element = array[i];
+        // Remove box
+    }
+}
 
 // Load grid on start
 const body = document.querySelector("body")
@@ -24,3 +46,28 @@ sketchBox.forEach((box) => {
         e.currentTarget.classList.add("sketch");
     })
 })
+
+// Change grid size
+
+let slider = document.querySelector(".slider");
+let sliderText = slider.querySelector("#rangeValue");
+slider = slider.querySelector("input");
+console.log(slider)
+let sliderButton = document.querySelector("#gridSize");
+
+sliderButton.addEventListener("click", (e) => {
+    let sldrVal = +slider.value;
+    let textVal = +sliderText.textContent;
+    if(sldrVal === textVal) console.log("Were the same");
+    else if(sldrVal < textVal) {
+        // girdRemove(currentVal, newVal)
+        console.log("Slider less than current")
+        sliderText.textContent = sldrVal
+    }
+    else if(sldrVal > textVal) {
+        // girdRemove(currentVal, newVal)
+        console.log("Slider greater than current")
+        sliderText.textContent = sldrVal
+    };
+});
+
