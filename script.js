515241b1077d9a46742bd990b43ecf9f8edd80cc
EzekielEnnis <143122905+EzekielEnnis@users.noreply.github.com>
2023-09-10 17:27:59 +0100

    Add ability to draw on grid
    Add event listener to the boxes and when moused over
    the class "sketch" gets added which has a black
    background color. May be temperary.

---
+++ script.js
@@ -0,0 +1,24 @@
+function createGrid(size){
+    let gridSize = size*size;
+
+// find percentage of space the box should take up (basis)
+    let basis = (size/gridSize)*100; 
+    const container = document.querySelector(".sketch-container");
+    for (let i = 0; i < gridSize; i++) {
+        const box = document.createElement("div");
+        box.classList.add("sketch-box");
+        // set basis dynamically when created to prevent mistakes
+        box.style.cssText = `flex-basis: ${basis}%;`;
+        container.appendChild(box);
+    };
+        console.log(container.children)
+};
+
+createGrid(15);
+
+let sketchBox = document.querySelectorAll(".sketch-box");
+sketchBox.forEach((box) => {
+    box.addEventListener("mouseover", (e)=> {
+        e.currentTarget.classList.add("sketch");
+    })
+})
