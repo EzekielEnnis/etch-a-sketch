cb393ba867e959d57921b740752fb352c5e0bb83
EzekielEnnis <143122905+EzekielEnnis@users.noreply.github.com>
2023-09-10 22:19:59 +0100

    Prep functions for new attempt

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 const body = document.querySelector("body")
+// Slider
+let slider = document.querySelector(".slider > input");
+let sliderText = document.querySelector("#rangeValue");
+let sliderButton = document.querySelector("#gridSize");
 
+function createBox(){
+    // Make box
+};
 
+function createGrid(){
+    // Make grid
+};
+
+function reset(){
+    // Clear Grid of Sketch
+};
+
+function apply(){
+    let sldrVal = +slider.value;
+    let textVal = +sliderText.textContent;
+    if(sldrVal === currentGridWidth) console.log("Were the same");
+    else console.log("Change")
+};
 
 // Load grid on start
-body.onload = createGrid(currentGridWidth)
+body.onload = createGrid(+sliderText.textContent)
 
 // Sketch
 sketchBox.forEach((box) => {
@@ -12,18 +33,8 @@ sketchBox.forEach((box) => {
     })
 })
 
-// Slider
-let slider = document.querySelector(".slider");
-let sliderText = slider.querySelector("#rangeValue");
-slider = slider.querySelector("input");
-let sliderButton = document.querySelector("#gridSize");
 
-sliderButton.addEventListener("click", (e) => {
-    let sldrVal = +slider.value;
-    let textVal = +sliderText.textContent;
-    if(sldrVal === currentGridWidth) console.log("Were the same");
-    else console.log("Change")
-});
+sliderButton.addEventListener("click", (e) => apply());
 
 // Called when slider changes
 function slide(){
