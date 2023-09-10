b38d32fb4ca05d88dad0c541ad9b3989315a5d99
EzekielEnnis <143122905+EzekielEnnis@users.noreply.github.com>
2023-09-10 22:12:18 +0100

    Clear Js and CSS of any logic concerning sketch
    Time to try a new approach. Remove everything
    except logic/css to do with slider, buttons, header
    and container

--- script.js
+++ script.js
@@ -1,40 +1,18 @@
+const body = document.querySelector("body")
 
-const container = document.querySelector(".sketch-container");
-let currentGridWidth = 16;
 
-// CreateBox
-function createBox(){
-    //Calculate basis %
-    let basis = (currentGridWidth/(currentGridWidth*currentGridWidth))*100; 
-    const box = document.createElement("div");
-    box.classList.add("sketch-box");
-    // set basis dynamically when created to prevent mistakes
-    box.style.flexBasis = `${basis}%`
-    return box;
-}
-
-// Create grid
-function createGrid(size){
-    let gridSize = size*size;
-
-    for (let i = 0; i < gridSize; i++) {
-        container.appendChild(createBox());
-    };
-};
 
 // Load grid on start
-const body = document.querySelector("body")
 body.onload = createGrid(currentGridWidth)
 
 // Sketch
-let sketchBox = document.querySelectorAll(".sketch-box");
 sketchBox.forEach((box) => {
     box.addEventListener("mouseover", (e)=> {
         e.currentTarget.classList.add("sketch");
     })
 })
 
-// Change grid size
+// Slider
 let slider = document.querySelector(".slider");
 let sliderText = slider.querySelector("#rangeValue");
 slider = slider.querySelector("input");
@@ -44,15 +22,7 @@ sliderButton.addEventListener("click", (e) => {
     let sldrVal = +slider.value;
     let textVal = +sliderText.textContent;
     if(sldrVal === currentGridWidth) console.log("Were the same");
-    else {
-        currentGridWidth = sldrVal;
-        let toDel = container.children.length;
-        for (let i = 0; i < toDel; i++) {
-            container.firstChild.remove();
-        }
-        createGrid();
-    };
-    
+    else console.log("Change")
 });
 
 // Called when slider changes
