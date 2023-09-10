acf2922b57dcba70a97de5fd7aad34268e37baed
EzekielEnnis <143122905+EzekielEnnis@users.noreply.github.com>
2023-09-10 21:54:15 +0100

    Need revert
    Tried to making append function but didnt work. Need revert
    possibly rewrite Js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function createBox(){
     const box = document.createElement("div");
     box.classList.add("sketch-box");
     // set basis dynamically when created to prevent mistakes
-    box.style.cssText = `flex-basis: ${basis}%;`;
+    box.style.flexBasis = `${basis}%`
     return box;
 }
 
@@ -21,27 +21,6 @@ function createGrid(size){
         container.appendChild(createBox());
     };
 };
-
-
-// Append box to grid
-function girdAppend(newSize) {
-    // How many boxes need to be added
-    let dif = (newSize*newSize)-(currentGridWidth*currentGridWidth)
-    for (let i = 0; i < dif; i++) {
-        const element = array[i];
-        container.appendChild()
-    }
-}
-
-// Remove box from grid
-function girdAppend(newSize) {
-    // How many boxes need to be added
-    let dif = (newSize*newSize)-(currentGridWidth*currentGridWidth)
-    for (let i = 0; i < dif; i++) {
-        const element = array[i];
-        // Remove box
-    }
-}
 
 // Load grid on start
 const body = document.querySelector("body")
@@ -56,31 +35,28 @@ sketchBox.forEach((box) => {
 })
 
 // Change grid size
-
 let slider = document.querySelector(".slider");
 let sliderText = slider.querySelector("#rangeValue");
 slider = slider.querySelector("input");
-console.log(slider)
 let sliderButton = document.querySelector("#gridSize");
-
-// Called when slider changes
-function slide(){
-    sliderText.textContent = +slider.value
-}
 
 sliderButton.addEventListener("click", (e) => {
     let sldrVal = +slider.value;
     let textVal = +sliderText.textContent;
     if(sldrVal === currentGridWidth) console.log("Were the same");
-    else if(sldrVal < currentGridWidth) {
+    else {
         currentGridWidth = sldrVal;
-        console.log("Slider less than current")
-        // girdRemove(newVal)
-    }
-    else if(sldrVal > currentGridWidth) {
-        currentGridWidth = sldrVal;
-        console.log("Slider greater than current")
-        // girdRemove(newVal)
+        let toDel = container.children.length;
+        for (let i = 0; i < toDel; i++) {
+            container.firstChild.remove();
+        }
+        createGrid();
     };
+    
 });
+
+// Called when slider changes
+function slide(){
+    sliderText.textContent = +slider.value
+}
 
